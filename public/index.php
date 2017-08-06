d247e8801aae3b4810951d7935613d8e23192c3d
Pavel Volyntsev <pavel@visualpharm.com>
2017-08-06 21:40:40 +0700

    [changed] удалил отладку

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ try
     $config = include(__DIR__ . '/../config/config.php');
 
     $connectionString = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
-    $db = new stdClass(); // new PDO($connectionString, $config['user'], $config['password']);
+    $db = new PDO($connectionString, $config['user'], $config['password']);
 
     // Получить информацию о текущем городе
-    $city = array('name' => 'XXX'); /* $db->query("SELECT * FROM city WHERE id = :id")
+    $city = $db->query("SELECT * FROM city WHERE id = :id")
                 ->execute(array(
                     'id' => $config['cityId'],
-                )); */
+                ));
 
     include __DIR__ . '/../layouts/' . $config['layout'] . '.php';
 } catch(\Exception $exception)
