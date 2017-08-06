3d99379b91c1f934a55b2876604e7e86f47e553e
Pavel Volyntsev <pavel@visualpharm.com>
2017-08-06 21:40:28 +0700

    [added] немного рекламы

--- layouts/city.php
+++ layouts/city.php
@@ -6,8 +6,27 @@
 <html>
 <head>
     <title>Новостной портал / Вакансии / Объявления города <?php echo $city['name'] ?></title>
+    <link rel="stylesheet" href="https://semantic-ui.com/dist/semantic.min.css">
+    <style type="text/css">
+        .ui.container:first-child {
+            margin-top: 3em;
+        }
+    </style>
 </head>
 <body>
-<h1>Добро пожаловать в <?php echo $city['name'] ?></h1>
+<div class="ui container">
+    <h1 class="ui header">Добро пожаловать в <?php echo $city['name'] ?></h1>
+
+    <div class="ui bottom fixed menu">
+        <div class="ui item"><img class="ui avatar image" src="http://copist.ru/wp-content/uploads/2013/09/19789.jpg"> Pavel Volyntsev</div>
+
+        <div class="ui right menu">
+            <a class="ui item" href="http://toster.ru/user/copist">toster.ru</a>
+            <a class="ui item" href="http://webmentor.pro/">webmentor.pro</a>
+            <a class="ui item" href="http://copist.ru/">copist.ru</a>
+            <a class="ui item webmentor" href="https://www.codementor.io/i/nz2q2ocre">codementor.io</a>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- layouts/error.php
+++ layouts/error.php
@@ -7,15 +7,38 @@
 <html>
 <head>
     <title>Ошибка</title>
+    <link rel="stylesheet" href="https://semantic-ui.com/dist/semantic.min.css">
+    <style type="text/css">
+        .ui.container:first-child {
+            margin-top: 3em;
+        }
+    </style>
 </head>
 <body>
-<h1>Произошла ошибка</h1>
-<?php if (isset($exception)) { ?>
-    <h2><?php echo $exception->getMessage() ?></h2>
-    <p><?php echo $exception->getFile() .':' . $exception->getLine() ?></p>
-    <pre><?php echo $exception->getTraceAsString() ?></pre>
-<?php } else { ?>
-    <h2><?php echo $errorMessage ?></h2>
-<?php } ?>
+<div class="ui container">
+    <h2 class="ui red header">
+        <i class="bug gray icon"></i>
+        Произошла ошибка
+    </h2>
+
+    <?php if (isset($exception)) { ?>
+        <h2><?php echo $exception->getMessage() ?></h2>
+        <p><?php echo $exception->getFile() .':' . $exception->getLine() ?></p>
+        <pre><?php echo $exception->getTraceAsString() ?></pre>
+    <?php } else { ?>
+        <h2><?php echo $errorMessage ?></h2>
+    <?php } ?>
+
+    <div class="ui bottom fixed menu">
+        <div class="ui item"><img class="ui avatar image" src="http://copist.ru/wp-content/uploads/2013/09/19789.jpg"> Pavel Volyntsev</div>
+
+        <div class="ui right menu">
+            <a class="ui item" href="http://toster.ru/user/copist">toster.ru</a>
+            <a class="ui item" href="http://webmentor.pro/">webmentor.pro</a>
+            <a class="ui item" href="http://copist.ru/">copist.ru</a>
+            <a class="ui item webmentor" href="https://www.codementor.io/i/nz2q2ocre">codementor.io</a>
+        </div>
+    </div>
+</div>
 </body>
 </html>
