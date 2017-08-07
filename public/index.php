95736aa11f9692197fa80589b25aea62f975079a
Pavel Volyntsev <pavel@visualpharm.com>
2017-08-07 18:10:02 +0700

    [changed] если в конфиге не указан ID города, то поиск по доменному имени

--- public/index.php
+++ public/index.php
@@ -8,10 +8,21 @@ try
     $db = new PDO($connectionString, $config['user'], $config['password']);
 
     // Получить информацию о текущем городе
-    $city = $db->query("SELECT * FROM city WHERE id = :id")
-                ->execute(array(
-                    'id' => $config['cityId'],
-                ));
+    if ($config['cityId'])
+    {
+        // если в конфиге указан конкретный город
+        $city = $db->query("SELECT * FROM city WHERE id = :id")
+            ->execute(array(
+                'id' => $config['cityId'],
+            ));
+    } else
+    {
+        // получить из таблицы городов по названию доменного имени
+        $city = $db->query("SELECT * FROM city WHERE domain = :domain")
+            ->execute(array(
+                'domain' => $_SERVER['SERVER_NAME'],
+            ));
+    }
 
     include __DIR__ . '/../layouts/' . $config['layout'] . '.php';
 } catch(\Exception $exception)
